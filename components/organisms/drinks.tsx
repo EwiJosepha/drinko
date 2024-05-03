0e2967a0a5864fa258587d6bba9be72280a67863
ewi josepha <ewijosepha@gmail.com>
2024-05-03 16:53:57 +0200

    adding view and edit pages

--- components/organisms/drinks.tsx
+++ components/organisms/drinks.tsx
@@ -4,253 +4,249 @@ import React, { useState } from "react";
 import DbLayout from "../db-header";
 
 type Prop = {
-  rooms: string;
-  bath: string;
-  livingRooms: string;
-  location: string;
-  kitchen: string;
-  areaInKm: number;
-  shortDescription: string;
-  agentId: number;
+  name: string;
+  description: string;
+  ingredients: string;
+  recipe: string[];
+  category: string;
+  glass: string;
+
 }
 
 const DrinkCard: React.FC = () => {
   const [error, setError] = useState<string>('');
   const [errorrooms, setErrorrooms] = useState<string>('');
-  const [errorbath, setErrorbath] = useState<string>('');
-  const [errorlivingrooms, setErrorlivingrooms] = useState<string>('');
-  const [errorlocation, setErrorlocation] = useState<string>('');
-  const [errorkitchen, setErrorkitchen] = useState<string>('');
-  const [errorarea, setErrorarea] = useState<string>('');
+  const [errorname, setErrorname] = useState<string>('');
+  const [errordescription, setErrordescription] = useState<string>('');
+  const [errorrecipe, setErrorrecipe] = useState<string>('');
+  const [erroringredients, setErroringredients] = useState<string>('');
+  const [errorglass, setErrorglass] = useState<string>('');
+  const [errorcategory, setErrorcategory] = useState<string>('');
   const [disable, setDisable] = useState(false)
 
   const [propertyInfo, setPropertyInfo] = useState<Prop>({
-    rooms: "",
-    bath: "",
-    livingRooms: "",
-    location: "",
-    kitchen: "",
-    areaInKm: 0,
-    shortDescription: "",
-    agentId: 0,
+    name: "",
+    description: "",
+    recipe: [],
+    ingredients: "",
+    glass: "",
+    category: "",
   })
 
   const [data, setData] = useState<Prop>(propertyInfo)
 
-  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleSelectChangename = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     setPropertyInfo((prevPropertyInfo) => ({
       ...prevPropertyInfo,
-      bath: value,
+      name: value,
     }));
 
-    setData((prevPropertyInfo) => ({
-      ...prevPropertyInfo,
-      bath: value,
+    // setData((prevPropertyInfo) => ({
+    //   ...prevPropertyInfo,
+    //   bath: value,
 
-    }));
-    setErrorbath('')
+    // }));
+    setErrorname('')
   };
 
-  const handleSelectChangelivingRooms = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleSelectChangedescription = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const { value } = e.target;
     setPropertyInfo((prevPropertyInfo) => ({
       ...prevPropertyInfo,
-      livingRooms: value,
+      description: value,
     }));
 
 
-    setData((prevPropertyInfo) => ({
-      ...prevPropertyInfo,
-      livingRooms: value,
-    }));
+    // setData((prevPropertyInfo) => ({
+    //   ...prevPropertyInfo,
+    //   livingRooms: value,
+    // }));
 
-    setErrorlivingrooms('')
+    setErrordescription('')
   };
 
-  const handleSelectChangekitchen = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+  const handleSelectChangeingredients = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const { value } = e.target;
     setPropertyInfo((prevPropertyInfo) => ({
       ...prevPropertyInfo,
-      kitchen: value,
+      ingredients: value,
     }));
 
     setData((prevPropertyInfo) => ({
       ...prevPropertyInfo,
-      kitchen: value,
+      ingredients: value,
     }));
-    setErrorkitchen('')
+    setErroringredients('')
   };
-  const handleSelectChangeRooms = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  // const handleSelectChangerecipe = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  //   const { value } = e.target;
+  //   setPropertyInfo((prevPropertyInfo) => ({
+  //     ...prevPropertyInfo,
+  //     recipe: value,
+  //   }));
+
+  //   setData((prevPropertyInfo) => ({
+  //     ...prevPropertyInfo,
+  //     recipe: value,
+  //   }));
+  //   setErrorrecipe('')
+  // };
+
+  const handleInputChangecategory = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const { value } = e.target;
     setPropertyInfo((prevPropertyInfo) => ({
       ...prevPropertyInfo,
-      rooms: value,
+      category: value,
     }));
 
-    setData((prevPropertyInfo) => ({
-      ...prevPropertyInfo,
-      rooms: value,
-    }));
-    setErrorrooms('')
+    // setData((prevPropertyInfo) => ({
+    //   ...prevPropertyInfo,
+    //   category: parseFloat(value),
+    // }));
+    setErrorcategory('')
   };
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChangeglass = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const { value } = e.target;
     setPropertyInfo((prevPropertyInfo) => ({
       ...prevPropertyInfo,
-      areaInKm: parseFloat(value),
+      glass: value
     }));
+    // setData((prevPropertyInfo) => ({
+    //   ...prevPropertyInfo,
+    //   glass: value
+    // }));
 
-    setData((prevPropertyInfo) => ({
-      ...prevPropertyInfo,
-      areaInKm: parseFloat(value),
-    }));
-    setErrorarea('')
-  };
-
-  const handleInputChangeLocation = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    setPropertyInfo((prevPropertyInfo) => ({
-      ...prevPropertyInfo,
-      location: value
-    }));
-    setData((prevPropertyInfo) => ({
-      ...prevPropertyInfo,
-      location: value
-    }));
-
-    setErrorlocation('')
+    setErrorglass('')
   }
 
   function save() {
-    if (propertyInfo.areaInKm === 0) {
-      setErrorarea('Area is required*')
+    if (propertyInfo.name === "") {
+      setErrorname('Name is required*')
       return
     }
 
-    if (propertyInfo.livingRooms === '') {
-      setErrorlivingrooms('Living room is requied*')
+    if (propertyInfo.description === '') {
+      setErrordescription('description is requied*')
       return
     }
-    if (propertyInfo.location === '' && propertyInfo.location.length > 50) {
-      setErrorlocation('location is required*')
+
+    if (!propertyInfo.recipe) {
+      setErrorrecipe('recipe is required*')
       return
     }
-    if (propertyInfo.kitchen === "") {
-      setErrorkitchen('kitchen is required*')
+    if (propertyInfo.glass === "") {
+      setErrorglass('Glass is required*')
       return
     }
-    if (propertyInfo.rooms === "") {
-      setErrorrooms('Room is required*')
+    if (propertyInfo.category === "") {
+      setErrorcategory('Room is required*')
       return
     }
-    if (propertyInfo.bath === '') {
-      setErrorbath('bath is required*')
-      return
-    }
+  
     setDisable(true)
   }
 
   return (
-    
-      <div className="p-4 shadow shadow-blue rounded-lg bg-neutral-700">
-        <h3 className="text-xl font-semibold mb-2">Adding Drinks</h3>
 
-        <div className='flex justify-between items-center'>
-          <div className="my-4 w-[45%]">
-            <label htmlFor="propertySize" className="block">
-              Name*
-            </label>
-            <input
-              type="text"
-              id="propertySize"
-              className="border border-gray-200 px-4 py-3 rounded-md w-full"
-              onChange={handleInputChange}
-              required
-            />
-            {errorarea && <p className="text-red-500 text-sm py-2">{errorarea}</p>}
-          </div>
+    <div className="p-4 shadow shadow-blue rounded-lg bg-neutral-700">
+      <h3 className="text-xl font-semibold mb-2">Adding Drinks</h3>
 
-          <div className="mb-4 w-[45%]">
-            <label htmlFor="bedrooms" className="block">
-              Bedrooms*
-            </label>
-            <select className="border border-gray-200 px-4 py-3 rounded-md w-full" onChange={handleSelectChangeRooms}>
-              <option value="">0</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-            </select>
-            {errorrooms && <p className="text-red-500 text-sm py-2">{errorrooms}</p>}
-
-          </div>
-        </div>
-        <div className='flex justify-between'>
-          <div className="mb-4 w-[45%]">
-            <label htmlFor="bathrooms" className="block">
-              Bathrooms*
-            </label>
-            <select className="border border-gray-200 px-4 py-3 rounded-md w-full" onChange={handleSelectChange}>
-              <option value="">0</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-            </select>
-            {errorbath && <p className="text-red-500 text-sm py-2">{errorbath}</p>}
-
-          </div>
-          <div className="mb-4 w-[45%]">
-            <label htmlFor="bathrooms" className="block">
-              Livingrooms*
-            </label>
-            <select className="border border-gray-200 px-4 py-3 rounded-md w-full" onChange={handleSelectChangelivingRooms}>
-              <option value="">0</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-            </select>
-            {errorlivingrooms && <p className="text-red-500 text-sm py-2">{errorlivingrooms}</p>}
-
-          </div>
-        </div>
-        <div className="mb-4">
-          <label htmlFor="kitchenDescription" className="block">
-            Kitchen*
-          </label>
-          <textarea
-            id="kitchenDescription"
-            onChange={handleSelectChangekitchen}
-            className="border border-gray-200 px-4 py-3 rounded-md w-full"
-            required
-          ></textarea>
-          {errorkitchen && <p className="text-red-500 text-sm py-2">{errorkitchen}</p>}
-
-        </div>
-        <div className="mb-4">
-          <label htmlFor="propertyLocation" className="block">
-            Location*
+      <div className='flex justify-between items-center'>
+        <div className="my-4 w-[45%]">
+          <label htmlFor="propertySize" className="block">
+            Name*
           </label>
           <input
             type="text"
-            id="propertyLocation"
+            id="propertySize"
             className="border border-gray-200 px-4 py-3 rounded-md w-full"
-            onChange={handleInputChangeLocation}
+            onChange={handleSelectChangename}
             required
           />
+          {errorname && <p className="text-red-500 text-sm py-2">{errorname}</p>}
         </div>
-        {errorlocation && <p className="text-red-500 text-sm py-2">{errorlocation}</p>}
 
-        <button disabled={disable} className='text-white w-40 bg-blue px-4 py-2 rounded-md mt-5 mb-3' onClick={save}>Save</button>
+        <div className="mb-4 w-[45%]">
+          <label htmlFor="bedrooms" className="block">
+            Description*
+          </label>
+          <select className="border border-gray-200 px-4 py-3 rounded-md w-full" onChange={ handleSelectChangedescription}>
+            <option value="">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+          {errorrooms && <p className="text-red-500 text-sm py-2">{errorrooms}</p>}
+
+        </div>
+      </div>
+      <div className='flex justify-between'>
+        <div className="mb-4 w-[45%]">
+          <label htmlFor="bathrooms" className="block">
+            Category*
+          </label>
+          <select className="border border-gray-200 px-4 py-3 rounded-md w-full" onChange={handleInputChangecategory}>
+            <option value="">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+          {errorcategory && <p className="text-red-500 text-sm py-2">{errorcategory}</p>}
+
+        </div>
+        <div className="mb-4 w-[45%]">
+          <label htmlFor="bathrooms" className="block">
+            Glass*
+          </label>
+          <select className="border border-gray-200 px-4 py-3 rounded-md w-full" onChange={handleInputChangeglass}>
+            <option value="">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+          {errorglass && <p className="text-red-500 text-sm py-2">{errorglass}</p>}
+
+        </div>
+      </div>
+      <div className="mb-4">
+        <label htmlFor="kitchenDescription" className="block">
+          Ingredients*
+        </label>
+        <textarea
+          id="kitchenDescription"
+          onChange={handleSelectChangeingredients}
+          className="border border-gray-200 px-4 py-3 rounded-md w-full"
+          required
+        ></textarea>
+        {erroringredients && <p className="text-red-500 text-sm py-2">{erroringredients}</p>}
 
       </div>
-  
+      <div className="mb-4">
+        <label htmlFor="propertyLocation" className="block">
+          Recipe*
+        </label>
+        <input
+          type="text"
+          id="propertyLocation"
+          className="border border-gray-200 px-4 py-3 rounded-md w-full"
+          // onChange={handleInputChangerecipe}
+          required
+        />
+      </div>
+      {errorrecipe && <p className="text-red-500 text-sm py-2">{errorrecipe}</p>}
+
+      <button disabled={disable} className='text-white w-40 bg-blue px-4 py-2 rounded-md mt-5 mb-3' onClick={save}>Save</button>
+
+    </div>
+
   );
 };
 export default DrinkCard
